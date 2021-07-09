274e1e88b582aff7e3ef30e4fd09fd45d794c03d
xyzkpsf <xyzkpsf@gmail.com>
2021-07-09 02:00:07 -0700

    backup before reverting to test fetch

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ let customerDB = [];
 let currCustomer;
 var map;
 
-// add a function that once trigger, return the reference of a customer
-
-// Need to handle nondefine
 const SERVICE = {
   name: "FreeAgentService",
   appletId: `aHR0cHM6Ly94eXprcHNmLmdpdGh1Yi5pby9Hb29nbGVNYXBzQXBwbGV0`,
@@ -107,64 +104,23 @@ const storeData = (data) => {
   });
 };
 
-// function initMap() {
-//   //geocoder = new google.maps.Geocoder();
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 12,
-//     center: { lat: 37.7749, lng: -122.4194 },
-//   });
-//   const interval = 750;
-//   console.log(customerDB);
-//   customerDB.forEach((customer, index) => {
-//     setTimeout(() => {
-//       geocodeAddress(geocoder, map, customer);
-//     }, index * interval);
-//   });
-// }
-
-// function geocodeAddress(geocoder, resultsMap, customer) {
-//   console.log(customer.fName, customer.lName);
-//   geocoder
-//     .geocode({ address: customer.address })
-//     .then(({ results }) => {
-//       resultsMap.setCenter(results[0].geometry.location);
-//       let marker = new google.maps.Marker({
-//         map: resultsMap,
-//         position: results[0].geometry.location,
-//       });
-//       google.maps.event.addListener(marker, "click", () => {
-//         FAClient.navigateTo(`/customers/view/${customer.id}`);
-//       });
-//       google.maps.event.addListener(marker, "mouseover", () => {
-//         document.getElementById(
-//           "info"
-//         ).textContent = `${customer.fName} ${customer.lName}, ${customer.company}, ${customer.phone}
-//         ${customer.address}`;
-//         let element = document.getElementsByClassName("customer_info");
-//         element[0].classList.add("show");
-//       });
-//       google.maps.event.addListener(marker, "mouseout", () => {
-//         let element = document.getElementsByClassName("customer_info");
-//         element[0].classList.remove("show");
-//       });
-//     })
-//     .catch((e) =>
-//       alert("Geocode was not successful for the following reason: " + e)
-//     );
-// }
-
 function initMap() {
+  //geocoder = new google.maps.Geocoder();
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: { lat: 37.7749, lng: -122.4194 },
   });
-  const geocoder = new google.maps.Geocoder();
-  if (currCustomer) {
-    geocodeAddress(geocoder, map, currCustomer);
-  }
+  const interval = 750;
+  console.log(customerDB);
+  customerDB.forEach((customer, index) => {
+    setTimeout(() => {
+      geocodeAddress(geocoder, map, customer);
+    }, index * interval);
+  });
 }
 
 function geocodeAddress(geocoder, resultsMap, customer) {
+  console.log(customer.fName, customer.lName);
   geocoder
     .geocode({ address: customer.address })
     .then(({ results }) => {
@@ -193,12 +149,3 @@ function geocodeAddress(geocoder, resultsMap, customer) {
       alert("Geocode was not successful for the following reason: " + e)
     );
 }
-
-function addMarker(location) {
-  const marker = new google.maps.Marker({
-    position: location,
-    map: map,
-  });
-}
-
-addMarker({ lat: 37.7749, lng: -122.4194 });
